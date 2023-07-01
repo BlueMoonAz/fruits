78fd387f474ce9fd341f4cf2323f1e3df618a952
Hiroshi Hanaoka <hiro_han@mxa.mesh.ne.jp>
2023-07-01 22:08:30 +0900

    make list page

--- resources/js/Pages/Fruits/List.vue
+++ resources/js/Pages/Fruits/List.vue
@@ -1,7 +1,54 @@
 <script setup>
-const testmsg='This is List.';
+import { onMounted,ref } from 'vue';
+import axios from 'axios';
+
+const items = ref(null);
+const itemsTotal = ref(0);
+const page=ref(1);
+const pageSize=10;
+const pagedItems = ref(null);
+
+const setPage = (val)=>{
+  page.value=val;
+  setPagedItems();
+}
+
+const setPagedItems = () => {
+  pagedItems.value=items.value.slice(pageSize * page.value - pageSize, pageSize * page.value);
+}
+
+onMounted(()=>{
+  axios.get('/api/list')
+            .then((res)=>{
+              items.value=res.data;
+              itemsTotal.value=items.value.length;
+              setPagedItems();
+    });
+})
 </script>
 
 <template>
-   <span v-text="testmsg"></span>
+    <el-table :data="pagedItems" style="width: 100%">
+    <el-table-column prop="id" label="id" width="80" />
+    <el-table-column prop="name" label="名称" />
+    <el-table-column prop="price" label="価格" width="100"
+      header-align="center" align="right">
+      <template #default="scope">
+        <div v-if="scope.row.price" >
+          {{scope.row.price.toLocaleString()}}
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column fixed="right" label="操作" width="120">
+      <template #default="scope">
+        <el-button link type="primary" >
+          編集
+        </el-button>
+        <el-button link type="primary" >
+          削除
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <el-pagination layout="prev, pager, next" :total="itemsTotal" @current-change="setPage"></el-pagination>
 </template>
